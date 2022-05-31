e7342ee72a2ef77785d653d27f2d4a9e4b0a0375
zhuang ma <zhuangzhimhao@yahoo.com>
2022-05-31 15:03:42 +0800

    new file:   query_sql.sql

---
+++ query_sql.sql
@@ -0,0 +1,5 @@
+select addvouch a, where  a.ctc=b.ctc   and a.tracode=b.tracode and a.cardno=b.cardno and b.wrongflag='0'
+select addvouch a, where  a.ctc=b.ctc   and a.tracode=b.tracode and a.cardno=b.cardno and b.wrongflag='0'
+where  a.ctc=b.ctc-1 and a.tracode=b.tracode and a.cardno=b.cardno and b.wrongflag='0' and a.my_m1=null and a.my_m2=null
+where  a.ctc=b.ctc-1 and a.tracode=b.tracode and a.cardno=b.cardno and b.wrongflag='0' and a.my_m1=null and a.my_m2=null
+
