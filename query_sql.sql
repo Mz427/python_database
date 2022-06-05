7431de42c5484a1e12a3959af14aa9cf4f092d46
MaZhuang <zhuangzhimhao@yahoo.com>
2022-06-05 13:09:32 +0800

    modified:   query_sql.sql 	modified:   sybase_iq_main.py

--- query_sql.sql
+++ query_sql.sql
@@ -5,80 +5,125 @@ select b.nodename as '网点名称', b.nodeno as '网点编码', a.cardno as '�
     when exists
     (
         select *
-        from addvouch as a1
+        from mz_addvouch as a1
         where a1.ctc = a.ctc and a1.tracode = a.tracode and a1.cardno = a.cardno and a1.wrongflag = '0'
         union
         select *
-        from carddetail as c1
+        from mz_carddetail as c1
         where c1.ctc = a.ctc and c1.tracode = a.tracode and c1.cardno = a.cardno and c1.wrongflag = '0'
     )
     then '写卡失败'
     when exists
     (
         select *
-        from addvouch as a2
+        from mz_addvouch as a2
         where a2.ctc = a.ctc + 1 and a2.tracode = a.tracode and a2.cardno = a.cardno and a2.wrongflag = '0'
         union
         select *
-        from carddetail as c2
+        from mz_carddetail as c2
         where c2.ctc = a.ctc + 1 and c2.tracode = a.tracode and c2.cardno = a.cardno and c2.wrongflag = '0'
     )
     then '写卡成功'
     when exists
     (
-        select a3.ctc, a3.balance
-        from oildetail as a3
+        select *
+        from mz_oildetail as a3
         where a3.cardno = a.cardno and a3.opetime > a.opetime and a3.suctag in ('00', '01', '02')
         union
-        select b3.ctc, b3.balance
-        from oilvouch as b3
+        select *
+        from mz_oilvouch as b3
         where b3.cardno = a.cardno and b3.opetime > a.opetime and b3.suctag in ('00', '01', '02')
         union
-        select c3.ctc, c3.balance
-        from unlocal_credit_vouch as c3
+        select *
+        from mz_unlocal_credit_vouch as c3
         where c3.cardno = a.cardno and c3.opetime > a.opetime and c3.suctag in ('00', '01', '02')
-    ) as new_oilvouch
+    ) 
     then
         case
         when exists
         (
             select *
-            from new_oilvouch as a4, cardaccount as b4
-            where a4.cardno = b4.cardno and a4.balance = b4.balance and a4.ctc =
+            from mz_cardaccount as b4, 
             (
-                select max(a4.ctc)
-                from a4
+            	select * 
+        		from mz_oildetail as a4
+        		where a4.cardno = a.cardno and a4.opetime > a.opetime and a4.suctag in ('00', '01', '02')
+        		union
+        		select *
+        		from mz_oilvouch as b4
+        		where b4.cardno = a.cardno and b4.opetime > a.opetime and b4.suctag in ('00', '01', '02')
+        		union
+        		select *
+        		from mz_unlocal_credit_vouch as c4
+        		where c4.cardno = a.cardno and c4.opetime > a.opetime and c4.suctag in ('00', '01', '02')
+            ) AS a5
+            where a5.cardno = b4.cardno and a5.balance = b4.balance and a5.ctc =
+            (
+                select max(a7.ctc)
+                from             
+                (
+            	select * 
+        		from mz_oildetail as a6
+        		where a6.cardno = a.cardno and a6.opetime > a.opetime and a6.suctag in ('00', '01', '02')
+        		union
+        		select *
+        		from mz_oilvouch as b6
+        		where b6.cardno = a.cardno and b6.opetime > a.opetime and b6.suctag in ('00', '01', '02')
+        		union
+        		select *
+        		from mz_unlocal_credit_vouch as c6
+        		where c6.cardno = a.cardno and c6.opetime > a.opetime and c6.suctag in ('00', '01', '02')
+            	) AS a7
             )
         )
         then '写卡成功'
         when exists
         (
             select *
-            from all_oildetail as a5, cardaccount as b5
-            where a5.cardno = b5.cardno and b5.balance - a5.balance = a.amount and a5.ctc =
+            from mz_cardaccount as b5,
             (
-                select max(a5.ctc)
-                from a5
+            	select * 
+        		from mz_oildetail as a8
+        		where a8.cardno = a.cardno and a8.opetime > a.opetime and a8.suctag in ('00', '01', '02')
+        		union
+        		select *
+        		from mz_oilvouch as b8
+        		where b8.cardno = a.cardno and b8.opetime > a.opetime and b8.suctag in ('00', '01', '02')
+        		union
+        		select *
+        		from mz_unlocal_credit_vouch as c8
+        		where c8.cardno = a.cardno and c8.opetime > a.opetime and c8.suctag in ('00', '01', '02')
+            ) as a9
+            where a9.cardno = b5.cardno and b5.balance - a9.balance = a.amount and a9.ctc =
+            (
+                select max(a10.ctc)
+                from            
+                (
+            		select * 
+        			from mz_oildetail as a11
+        			where a11.cardno = a.cardno and a11.opetime > a.opetime and a11.suctag in ('00', '01', '02')
+        			union
+        			select *
+        			from mz_oilvouch as b11
+        			where b11.cardno = a.cardno and b11.opetime > a.opetime and b11.suctag in ('00', '01', '02')
+        			union
+        			select *
+        			from mz_unlocal_credit_vouch as c11
+        			where c11.cardno = a.cardno and c11.opetime > a.opetime and c11.suctag in ('00', '01', '02')
+            	) AS a10
             )
         )
         then '写卡失败'
-        else null
-        end
-    else
-        '待确认卡钱包'
-    end as '原因分析',
-    case '原因分析'
-        when '写卡成功' then '待确认'
-        when '写卡失败' then '待冲正'
-        when '待确认卡钱包' then '待跟踪'
-        else null
-    end as '处理状态',
+        else null 
+        END
+    ELSE NULL
+    END as '处理状态',
     b.sinopec_nodeno as '石化编码'
 from addvouch as a, nodeinfor as b 
 where a.nodeno = b.nodeno and a.wrongflag = 'X' and
     convert(numeric(8, 0), convert(char(8), a.opetime,112)) = convert(numeric(8, 0), convert(char(8), dateadd(day, -2, getdate()), 112))
-order by terminalno
 go
+-------------------------------------------------------------------------------------------------------------------------
 SELECT *
 FROM pg_catalog.pg_tables pt 
 
@@ -126,3 +171,108 @@ CREATE TABLE mz_unlocal_credit_vouch
 	balance NUMERIC (12, 2),
 	wrongflag CHAR (1)
 )
+SELECT *
+FROM pg_catalog.pg_tables pt 
+
+CREATE TABLE mz_addvouch
+(
+	cardno CHAR (19),
+	compno CHAR (12),
+	opetime timestamp,
+	tracode CHAR (2),
+	ctc INT,
+	ttc INT,
+	amount NUMERIC (12, 2),
+	balance NUMERIC (12, 2),
+	wrongflag CHAR (1)
+);
+INSERT INTO mz_addvouch VALUES
+('1000119000000123451', '900000000001', '20220101 01:00:00', '31', 1, 1234, 1000, 1000, 'X'),
+('1000119000000123452', '900000000002', '20220102 02:00:00', '31', 2, 1234, 2000, 1000, 'X'),
+('1000119000000123453', '900000000003', '20220103 03:00:00', '31', 3, 1234, 3000, 1000, 'X'),
+('1000119000000123454', '900000000004', '20220104 04:00:00', '31', 4, 1234, 4000, 1000, 'X'),
+('1000119000000123455', '900000000005', '20220105 05:00:00', '31', 5, 1234, 5000, 1000, 'X'),
+('1000119000000123456', '900000000006', '20220106 06:00:00', '31', 6, 1234, 6000, 1000, 'X');
+
+CREATE TABLE mz_carddetail
+(
+	cardno CHAR (19),
+	compno CHAR (12),
+	opetime timestamp,
+	tracode CHAR (2),
+	ctc INT,
+	ttc INT,
+	amount NUMERIC (12, 2),
+	balance NUMERIC (12, 2),
+	wrongflag CHAR (1)
+);
+INSERT INTO mz_carddetail VALUES
+('1000119000000123451', '900000000001', '20220101 01:00:00', '31', 1, 1234, 1000, 1000, '0'),
+('1000119000000123452', '900000000002', '20220102 02:00:00', '31', 3, 1234, 2000, 1000, '0'),
+('1000119000000123453', '900000000003', '20220103 03:00:00', '31', 0, 1234, 3000, 1000, 'X'),
+('1000119000000123454', '900000000004', '20220104 04:00:00', '31', 0, 1234, 4000, 1000, 'X'),
+('1000119000000123455', '900000000005', '20220105 05:00:00', '31', 0, 1234, 5000, 1000, 'X'),
+('1000119000000123456', '900000000006', '20220106 06:00:00', '31', 0, 1234, 6000, 1000, 'X');
+
+CREATE TABLE mz_cardaccount
+(
+	cardno CHAR (19),
+	compno CHAR (12),
+	balance NUMERIC (12, 2),
+	prebalance NUMERIC (12, 2)
+);
+INSERT INTO mz_cardaccount VALUES
+('1000119000000123451', '900000000001', 1000, 1000),
+('1000119000000123452', '900000000002', 2000, 1000),
+('1000119000000123453', '900000000003', 1500, 1000),
+('1000119000000123454', '900000000004', 4000, 1000),
+('1000119000000123455', '900000000005', 5000, 1000),
+('1000119000000123456', '900000000006', 3000, 1000);
+
+CREATE TABLE mz_oildetail
+(
+	ttc INT,
+	suctag CHAR (2),
+	cardno CHAR (19),
+	opetime timestamp,
+	ctc INT,
+	amount NUMERIC (12, 2),
+	balance NUMERIC (12, 2),
+	wrongflag CHAR (1)
+);
+INSERT INTO mz_oildetail VALUES
+(1234, '01', '1000119000000123451', '20220101 01:00:00', 1, 1000, 0, '0'),
+(1234, '01', '1000119000000123452', '20220102 02:00:00', 2, 2000, 0, '0'),
+(1234, '01', '1000119000000123453', '20220107 03:00:00', 3, 1500, 1500, '0'),
+(1234, '01', '1000119000000123454', '20220108 04:00:00', 4, 1100, 0, '0'),
+(1234, '01', '1000119000000123455', '20210105 05:00:00', 5, 5000, 1000, '0'),
+(1234, '01', '1000119000000123456', '20210106 06:00:00', 6, 6000, 1000, '0');
+
+CREATE TABLE mz_oilvouch
+(
+	ttc INT,
+	suctag CHAR (2),
+	cardno CHAR (19),
+	opetime timestamp,
+	ctc INT,
+	amount NUMERIC (12, 2),
+	balance NUMERIC (12, 2),
+	wrongflag CHAR (1)
+);
+
+CREATE TABLE mz_unlocal_credit_vouch
+(
+	ttc INT,
+	suctag CHAR (2),
+	cardno CHAR (19),
+	opetime timestamp,
+	ctc INT,
+	amount NUMERIC (12, 2),
+	balance NUMERIC (12, 2),
+	wrongflag CHAR (1)
+);
+INSERT INTO mz_unlocal_credit_vouch  VALUES
+(1234, '01', '1000119000000123455', '20210205 05:00:00', 6, 5000, 0, '0'),
+(1234, '01', '1000119000000123456', '20210206 06:00:00', 7, 6000, 3000, '0');
+
+
--- sybase_iq_main.py
+++ sybase_iq_main.py
@@ -1,28 +1,28 @@
-import sqlanydb
-import psycopg2
-import openpyxl
+#import sqlanydb
+#import openpyxl
+import psycopg
 
-#Connetc query01.
-query_connect = sqlanydb.connect(dsn="query01")
-query_cursor = query_connect.cursor()
+##Connetc query01.
+#query_connect = sqlanydb.connect(dsn="query01")
+#query_cursor = query_connect.cursor()
 #Connetc postgresql.
-postgresql_connect = psycopg2.connect(database="mzdb", user="mz", password="058474", \
-    host="10.199.194.97" , port="5432")
+postgresql_connect = psycopg.connect(database="mzdb", user="mz", password="058474", \
+    host="10.9.7.201" , port="5432")
 postgresql_cursor = postgresql_connect.cursor()
 
 query_sql = " \
-    select cardno, compno, opetime, amount, a.balance, tracode, ctc, ttc, terminalno, wrongflag \
+    select * \
     from addvouch \
-    where wrongflag = 'X' and \
-        convert(numeric(8, 0), convert(char(8), opetime,112)) = convert(numeric(8, 0), convert(char(8), dateadd(day, -2, getdate()), 112))"
+    where wrongflag = 'X';"
+
 postgresql_sql = " \
     SELECT * \
     FROM pg_catalog.pg_tables pt;" 
-query_cursor.execute(query_sql)
+#query_cursor.execute(query_sql)
 postgresql_cursor.execute(postgresql_sql)
 #print(postgresql_cursor.fetchone())
 
-query_cursor.close()
-query_connect.close()
+#query_cursor.close()
+#query_connect.close()
 postgresql_cursor.close()
 postgresql_connect.close()
